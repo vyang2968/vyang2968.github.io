78a57541fdd7bd361624c85c025511746323c536
vyang <v.yang2968@gmail.com>
2024-02-09 00:14:00 -0500

    seperate part renames + nobutton shifting works

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-onload = () => {
-  const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-    clearTimeout(c);
-  }, 1000);
-};
+function randomMovement(element) {
+    var i = Math.floor(Math.random()* (window.screen.width - element.offsetWidth))+1;
+    var j = Math.floor(Math.random()* (window.screen.height - 150 - element.offsetHeight))+1;
+    element.style.position = "absolute";
+    element.style.left = i+"px";
+    element.style.top = j+"px";
+}
