47768dc912db3fe6028d001257eb9786b8d01acd
vyang2968 <v.yang2968@gmail.com>
2026-08-17 16:31:34 -0400

    delete everything

--- script.js
+++
@@ -1,7 +0,0 @@
-function randomMovement(element) {
-    var i = Math.floor(Math.random()* (window.screen.width - element.offsetWidth))+1;
-    var j = Math.floor(Math.random()* (window.screen.height - 150 - element.offsetHeight))+1;
-    element.style.position = "absolute";
-    element.style.left = i+"px";
-    element.style.top = j+"px";
-}
